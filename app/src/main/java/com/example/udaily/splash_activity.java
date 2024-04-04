b7324fe018a256c76a2bdeb74503eda1a9ec8637
PRATIK DIPAK PATIL <coolpratik9575@gmail.com>
2024-04-04 22:41:47 +0530

    bug fixing

--- app/src/main/java/com/example/udaily/splash_activity.java
+++ app/src/main/java/com/example/udaily/splash_activity.java
@@ -1,9 +1,9 @@
 package com.example.udaily;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class splash_activity extends AppCompatActivity {
 
